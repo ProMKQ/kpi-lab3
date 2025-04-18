84d276848e56c79fd1b9dad9a35b8e2244a706e8
ProMKQ <maximik128@gmail.com>
2025-04-19 01:38:33 +0300

    Final fixes

--- scripts/green_frame.sh
+++ scripts/green_frame.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST http://localhost:17000/ -d $'white\nbgrect 0.25 0.25 0.75 0.75\ngreen\nupdate'
+curl -X POST http://localhost:17000/ -d $'green\nbgrect 0.25 0.25 0.75 0.75\nupdate'
--- scripts/move_figure.sh
+++ scripts/move_figure.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-curl -X POST http://localhost:17000/ -d $'reset\nwhite\nfigure 0.50 0.50\nupdate'
-sleep 1
-curl -X POST http://localhost:17000/ -d $'reset\nwhite\nfigure 0.53 0.53\nupdate'
-sleep 1
-curl -X POST http://localhost:17000/ -d $'reset\nwhite\nfigure 0.56 0.56\nupdate'
-sleep 1
-curl -X POST http://localhost:17000/ -d $'reset\nwhite\nfigure 0.59 0.59\nupdate'
-sleep 1
-curl -X POST http://localhost:17000/ -d $'reset\nwhite\nfigure 0.62 0.62\nupdate'
+positions=("0.50" "0.53" "0.56" "0.59" "0.62")
+
+for pos in "${positions[@]}"
+do
+    curl -X POST http://localhost:17000/ -d $'reset\nwhite\nfigure '"$pos $pos"$'\nupdate'
+    sleep 1
+done
